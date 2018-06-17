f556b5ad4c686ba035e319a10371775182a7a3ad
Graham Allen <grahamallen@Grahams-MacBook-Pro.local>
2018-06-17 13:16:55 -0700

    Initial Commit

---
+++ audiounit-test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  audiounit-test
+//
+//  Created by Graham Allen on 6/17/18.
+//  Copyright © 2018 Graham Allen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
